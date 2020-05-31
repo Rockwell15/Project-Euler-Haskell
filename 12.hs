e4d5e4510a23c096516536a5a5a0b2bfb74efad4
Andrew Rockwell <rockwell15@hotmail.com>
2020-05-31 08:37:31 -0400

    moving \divisor count\ into Extras for repeat use

--- 12.hs
+++ 12.hs
@@ -1,6 +1,4 @@
+import Extras
 
 -- answer is first triangle number where it has > 500 divisors
 answers = head [ x | x <- scanl1 (+) [1..], 500 < divisorCount x ]
-
-divisorCount :: Int -> Int
-divisorCount x = sum [ 1 | n <- [1..floor $ sqrt $ fromIntegral x], x `mod` n == 0 ] * 2
--- packages/Extras/src/Extras.hs
+++ packages/Extras/src/Extras.hs
@@ -1,3 +1,6 @@
 module Extras (digitSum) where
 
 digitSum n = sum [ read [x] | x <- show $ n ]
+
+divisorCount :: Int -> Int
+divisorCount x = sum [ 1 | n <- [1..floor $ sqrt $ fromIntegral x], x `mod` n == 0 ] * 2
