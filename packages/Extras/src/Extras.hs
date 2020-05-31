9c52e80712673a2a1acc4fcd4eb6baedad455696
Andrew Rockwell <rockwell15@hotmail.com>
2020-05-31 08:54:22 -0400

    exposing functions

--- packages/Extras/src/Extras.hs
+++ packages/Extras/src/Extras.hs
@@ -1,6 +1,9 @@
-module Extras (digitSum) where
+module Extras (digitSum, divisors, divisorCount) where
 
 digitSum n = sum [ read [x] | x <- show $ n ]
 
+divisors :: Int -> [Int]
+divisors x = [ n | n <- [1..x], x `mod` n == 0 ]
+
 divisorCount :: Int -> Int
-divisorCount x = sum [ 1 | n <- [1..floor $ sqrt $ fromIntegral x], x `mod` n == 0 ] * 2
+divisorCount x = sum [ 1 | n <- divisors $ floor $ sqrt $ fromIntegral x ] * 2
