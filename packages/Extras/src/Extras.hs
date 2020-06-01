43256bd2374460699afa1fa63fc562ae30d6f64e
Andrew Rockwell <rockwell15@hotmail.com>
2020-06-01 14:09:11 -0400

    adding explod &&removechar to Extras package

--- packages/Extras/src/Extras.hs
+++ packages/Extras/src/Extras.hs
@@ -1,4 +1,4 @@
-module Extras (digitSum, divisors, divisorCount) where
+module Extras (digitSum, divisors, divisorCount, removechar, explode) where
 
 digitSum n = sum [ read [x] | x <- show $ n ]
 
@@ -7,3 +7,17 @@ divisors x = [ n | n <- [1..x], x `mod` n == 0 ]
 
 divisorCount :: Int -> Int
 divisorCount x = sum [ 1 | n <- divisors $ floor $ sqrt $ fromIntegral x ] * 2
+
+removechar :: Char -> String -> String
+removechar c string = [ x | x <- string, c /= x ]
+
+explode :: Char -> String -> [String]
+explode delim string =
+  foldr
+  (\c acc ->
+    if c == delim 
+      then "":acc 
+      else (( [c] ++ head acc ):tail acc )
+  )
+  [""]
+  string
